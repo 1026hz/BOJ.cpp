f1c06bb60ac1185bd3305de56a1105282331acd7
HeeJae <132039586+1026hz@users.noreply.github.com>
2024-09-23 13:25:31 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4196 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int pre[5001];
+int nxt[5001];
+vector<int> v;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    queue<int> q;
+    int n;
+    cin >> n;
+
+    for(int i=1; i<=n; i++) q.push(i);
+
+    while (q.size() > 1){
+        q.pop();
+        int tmp = q.front();
+        q.pop();
+        q.push(tmp);
+    }
+    cout << q.front();
+    
+}
