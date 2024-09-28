f0adebe7cd479217dbc1ee1860e763d6658ae15e
HeeJae <132039586+1026hz@users.noreply.github.com>
2024-09-28 22:29:32 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n=0;
+    cin >> n;
+    string s;
+    
+    for(int i=1; i<=n; i++){
+        s += "*";
+        cout << s << '\n';
+    }
+
+}
